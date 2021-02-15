ad77f9ed1644ab4ee6e6c89b882e950ec95d40aa
digenets <digenets@gmail.com>
2021-02-15 17:01:02 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/Engineering_calc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Engineering_calc.dir/main.c.obj"
+  "Engineering_calc.exe"
+  "Engineering_calc.exe.manifest"
+  "Engineering_calc.pdb"
+  "libEngineering_calc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Engineering_calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
